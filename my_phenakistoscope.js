3bbddbcab9b248e0e9173b9fae01504bd68e43e6
sroedel <sproedel14@gmail.com>
2023-08-17 11:27:51 +1200

    Test

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,47 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 15;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("spin" , "png");
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 10);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
-
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
-}
-
-function faces(x, y, animation, pScope){
+  var layer1 = new PLayer(Spin);
+  layer1.mode(RING);
+  layer1.set_boundary( 0, 300 );
   
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  var effect = new PLayer(dots);
+  effect.mode(SWIRL(10));
+  effect.set_boundary( 0, 1000 );
+  
 
 }
 
-function squares(x, y, animation, pScope){
+function Spin(x, y, animation, pScope){
+  push()
+  scale(3)
+  if(animation.frame == 0){
+  pScope.draw_image("spin",x,y);
+  }
+  pop()
+  //translate(50 * animation.frame, 0);
+  //scale(animation.frame*2);
+  // fill(196, 252, 255)
+  
+  // let ballSize  = 100 + (animation.wave(1)* 20)
+  // let bouce = 50* animation.wave()
+  // ellipse(150, 850+bouce ,ballSize); 
+  
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
+}
+function dots(x, y, animation, pScope){
+fill(225)
+  ellipse(0,0,10, 10)
 }
