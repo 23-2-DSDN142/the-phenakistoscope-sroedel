f02826806585f9e6ec68e9831a3cca95eb1543a0
sroedel <sproedel14@gmail.com>
2023-09-04 10:31:39 +1200

    Added Duck

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,47 +1,33 @@
 const SLICE_COUNT = 15;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("spin" , "png");
+  pScope.load_image("duck" , "png");
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 10);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null,51, 128, 145);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(Spin);
+  var layer1 = new PLayer(duck);
   layer1.mode(RING);
-  layer1.set_boundary( 0, 300 );
-  
-  var effect = new PLayer(dots);
-  effect.mode(SWIRL(10));
-  effect.set_boundary( 0, 1000 );
+  layer1.set_boundary( 0, 800 );
   
 
 }
 
-function Spin(x, y, animation, pScope){
-  push()
-  scale(3)
-  if(animation.frame == 0){
-  pScope.draw_image("spin",x,y);
-  }
-  pop()
+function duck(x, y, animation, pScope){
   //translate(50 * animation.frame, 0);
   //scale(animation.frame*2);
-  // fill(196, 252, 255)
+ 
+  let ballSize  = 100 + (animation.wave(1)* 20)
+  let bouce = 50* animation.wave()
   
-  // let ballSize  = 100 + (animation.wave(1)* 20)
-  // let bouce = 50* animation.wave()
-  // ellipse(150, 850+bouce ,ballSize); 
-  
+  scale(.5) 
+  pScope.draw_image("duck",40,-1800+bouce,ballSize);
 
-}
-function dots(x, y, animation, pScope){
-fill(225)
-  ellipse(0,0,10, 10)
 }
