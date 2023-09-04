38519481de1bd55381a30d0264f50ff261958056
sroedel <sproedel14@gmail.com>
2023-09-04 12:09:57 +1200

    Added Rotating flower

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,33 +1,64 @@
-const SLICE_COUNT = 15;
-
 function setup_pScope(pScope){
-  pScope.output_mode(OUTPUT_GIF(1000));
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
-  pScope.set_direction(CCW);
-  pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("duck" , "png");
+  pScope.set_direction(CW);
+  pScope.set_slice_count(8);
+  pScope.load_image_sequence("chest_open", "png", 24);
+  pScope.load_image("sunflower","png")
 }
 
 function setup_layers(pScope){
+  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null,60,70,100);
 
-  new PLayer(null,51, 128, 145);  //lets us draw the whole circle background, ignoring the boundaries
+  let insideBackground = new PLayer(insideBlue);
+  insideBackground.mode(RING);
+  insideBackground.set_boundary(0,550);
 
-  var layer1 = new PLayer(duck);
-  layer1.mode(RING);
-  layer1.set_boundary( 0, 800 );
-  
+  let backgroundDetail = new PLayer(bubbles);
+  backgroundDetail.mode(SWIRL(5));
+  backgroundDetail.set_boundary(650,1000);
 
+  let outerRing = new PLayer(outsideRing);
+  outerRing.mode(RING);
+  outerRing.set_boundary(930,1000);
+
+  let chestSequence = new PLayer(chest);
+  chestSequence.mode(RING);
+  chestSequence.set_boundary(0,1000);
+
+  let CenterImage = new PLayer(sunflower);
+  CenterImage.mode(RING);
+  CenterImage.set_boundary(0,30);
 }
 
-function duck(x, y, animation, pScope){
-  //translate(50 * animation.frame, 0);
-  //scale(animation.frame*2);
+function sunflower(x,y,animation,pScope){
+  scale(3.5);
+  if(animation.frame == 0.1){
+  pScope.draw_image("sunflower",x,y);
+} 
+}
+
+function insideBlue (x,y,animation,pScope){
+  pScope.fill_background("blue");
+}
+function outsideRing (x,y,animation,pScope){
+    pScope.fill_background("purple");
+}
+function chest(x,y,animation,pScope){
+  translate(x,y-650);
+  scale(6);
+
+  pScope.draw_image_from_sequence("chest_open", 0, 0, animation.frame);
  
-  let ballSize  = 100 + (animation.wave(1)* 20)
-  let bouce = 50* animation.wave()
-  
-  scale(.5) 
-  pScope.draw_image("duck",40,-1800+bouce,ballSize);
+}
+
+function bubbles(x,y,animation,pScope){
+  let mvmnt =  70 *animation.frame;
+  let bubbleSize =60;
+  noStroke();
+  fill(255,0,155);
+  ellipse(x,y,bubbleSize,bubbleSize-mvmnt);
 
 }
