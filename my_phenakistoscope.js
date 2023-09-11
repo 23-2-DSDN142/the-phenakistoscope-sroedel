20a1917bb947c0a3cb132c1f15b6927d3adc445c
sroedel <sproedel14@gmail.com>
2023-09-11 12:14:33 +1200

    Blinking Stars

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,19 +1,18 @@
 
 function setup_pScope(pScope){
-  pScope.output_mode(OUTPUT_GIF(1000));
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(8);
   pScope.load_image_sequence("moon_cycle","png",8);
-  pScope.load_image("sunflower","png");
   pScope.load_image("Earth","png");
   
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null,60,70,100);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null,22, 27, 54);  //lets us draw the whole circle background, ignoring the boundaries
 
   let moonsequence = new PLayer(moon);
   moonsequence.mode(RING);
@@ -22,6 +21,14 @@ function setup_layers(pScope){
   let earthsequence = new PLayer(earth);
   earthsequence.mode(RING);
   earthsequence.set_boundary(1000,1000);
+
+  let Wstarsequence = new PLayer(whitestar);
+  Wstarsequence.mode(RING);
+  Wstarsequence.set_boundary( 0, 1000 );
+
+  let Ystarsequence = new PLayer(yellowstar);
+  Ystarsequence.mode(RING);
+  Ystarsequence.set_boundary( 0, 1000 );
 
 }
 
@@ -38,5 +45,31 @@ function moon(x,y,animation,pScope){
     } 
     }
 
+    function whitestar(x, y, animation, pScope){
+      let pulse  =(animation.wave(1)* 5)
+      let startcolor=color(255, 255, 255);
+      let endcolor =color(22, 27, 54);
+      let animatingcolor = lerpColor(startcolor,endcolor,animation.frame)
+      
+      fill(animatingcolor);
+      ellipse(190, 700,35+pulse); 
+      ellipse(10,400,20+pulse);
+      ellipse(270,400,30+pulse);
+      ellipse(450,650,25+pulse);
+      ellipse(150,550,25+pulse);
+}
 
+function yellowstar(x, y, animation, pScope){
 
+  let pulse2  =(animation.wave(1)* 3)
+  let startcolor=color(22, 27, 54);
+  let endcolor =color(250, 222, 167);
+  let animatingcolor = lerpColor(startcolor,endcolor,animation.wave(1))
+  
+  fill(animatingcolor);
+
+  ellipse(70, 800,20+pulse2); 
+  ellipse(100,450,30+pulse2);
+  ellipse(200,320,15+pulse2);
+  ellipse(300,550,25+pulse2);
+}
